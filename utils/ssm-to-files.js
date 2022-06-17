91cdfc304d8951a3b640fa3e103403af2e582ced
Tùng Nguyễn Sơn <sontung0@gmail.com>
2022-06-17 18:48:24 +0700

    Update ssm-to-files.js

--- utils/ssm-to-files.js
+++ utils/ssm-to-files.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
 function ssmToFiles(ssmInput, ssmOutput) {
-    let items = JSON.parse(fs.readFileSync(ssmInput, 'utf8'));
+    let ssm = JSON.parse(fs.readFileSync(ssmInput, 'utf8'));
 
-    for (const item of items) {
+    for (const item of ssm.Parameters) {
         let file = `${ssmOutput}${item.Name}`;
         fs.writeFileSync(file, item.Value);
         console.log(`Writed: ${file}`);
